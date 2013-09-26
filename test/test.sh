7f28c7e434a6820bf6804988f937ea0c2729e626
Chad Whitacre <chad@zetaweb.com>
2013-09-25 20:14:03 -0400

    Add an arg to test.sh, for the style number
    Now you can do `./test/test.sh 18` or `./test.sh 18`.

--- test/test.sh
+++ test/test.sh
@@ -10,8 +10,15 @@ cd $(dirname $0)
 failures=0
 total=0
 mydir=$(pwd)
+if [ -z "$1" ]; then
+    styles_to_test=$(echo ../[0-9][0-9]-*)
+else
+    styles_to_test=../$1-*
+fi
+
 files=$(echo *.txt)
-for dir in ../[0-9][0-9]-* ; do
+
+for dir in $styles_to_test ; do
     cd $dir
     for file in $files ; do
         for exe in * ; do
