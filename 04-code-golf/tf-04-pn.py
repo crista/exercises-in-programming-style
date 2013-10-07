872eabffdbbd09fc6470802e35f078a87e041e52
Crista Lopes <crista@tagide.com>
2013-10-07 14:57:15 -0700

    I just broke my rules about not adding more than one program in one language, but this code has gotta go in: contribution by Peter Norvig via email. Thank you!

---
+++ 04-code-golf/tf-04-pn.py
@@ -0,0 +1,10 @@
+# My golf score is slightly lower!  
+# Best wishes, Peter Norvig
+
+import re, sys, collections
+
+stopwords = set(open('../stop_words.txt').read().split(','))
+words = re.findall('[a-z]{2,}', open(sys.argv[1]).read().lower())
+counts = collections.Counter(w for w in words if w not in stopwords)
+for (w, c) in counts.most_common(25):
+    print w, '-', c
