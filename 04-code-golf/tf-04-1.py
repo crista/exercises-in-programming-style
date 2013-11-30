3f382c94923b14452ab764a623c5bd8c2a22199a
Crista Lopes <crista@tagide.com>
2013-11-30 10:14:45 -0800

    Added one more code golf version (actually renamed the previous one)

---
+++ 04-code-golf/tf-04-1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import re, string, sys
+
+stops = set(open("../stop_words.txt").read().split(",") + list(string.ascii_lowercase))
+words = [x.lower() for x in re.split("[^a-zA-Z]+", open(sys.argv[1]).read()) if len(x) > 0 and x.lower() not in stops]
+unique_words = list(set(words))
+unique_words.sort(lambda x, y: cmp(words.count(y), words.count(x)))
+print "\n".join(["%s - %s" % (x, words.count(x)) for x in unique_words[:25]])
