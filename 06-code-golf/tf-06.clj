cfa820c3945ae1053bff829890035b6d1c3dbf81
Eugenia Gabrielova <genia.likes.science@gmail.com>
2014-01-21 21:48:26 -0800

    Adds Java interop and changes remove to checking set

--- 06-code-golf/tf-06.clj
+++ 06-code-golf/tf-06.clj
@@ -1,11 +1,9 @@
 ":";exec java -cp "$HOME/.m2/repository/org/clojure/clojure/1.5.1/clojure-1.5.1.jar" clojure.main $0 $*
 
-(require '[clojure.string :as s])
-
 (doseq [c (take 25 
             (sort-by val > 
               (frequencies
                 (remove 
-                  #(some (partial = %) (s/split (slurp "../stop_words.txt") #","))
-                  (re-seq #"[a-z]{2,}" (s/lower-case (slurp (first *command-line-args*))))))))]
+                  #(contains? (set (.split (slurp "../stop_words.txt") ",")) %)
+                  (re-seq #"[a-z]{2,}" (.toLowerCase (slurp (first *command-line-args*))))))))]
   (printf "%s - %d\n" (first c) (nth c 1)))
